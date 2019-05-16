77f1b4481ad7078e2b8c80a523aa68035a998513
Olivia Silva <olivia.silva@wework.com>
2019-05-16 14:18:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -42,7 +42,11 @@ function viewCart() {
 
 
 function total() {
-  // write your code here
+ let totalCost = 0;
+  for (let i = 0; i < cart.length; i++){
+    totalCost += cart[i].itemPrice;
+  }
+  return totalCost;
 }
 
 function removeFromCart(item) {
